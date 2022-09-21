f0a93c022a30495d60e30a1f0dceb2ee856aa530
Gede Gery Sastrawan <gerysastrawan123@gmail.com>
2022-09-21 17:30:17 +0800

    feat: footer, gallery

---
+++ components/section/gallery-section.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Masonry from "react-masonry-css";
+import galleryData from "../data/gallery-data";
+
+const GallerySection = () => {
+  const breakpointColumnsObj = {
+    default: 3,
+    1100: 3,
+    700: 1,
+    500: 1,
+  };
+  const masonryOptions = {
+    breakpointCols: breakpointColumnsObj,
+    className: "my-masonry-grid",
+    columnClassName: "my-masonry-grid_column",
+  };
+  return (
+    <section id="gallery" className="p-32">
+      <div className="max-w-[1220px] mx-auto">
+        <h2 className="font-bold text-black text-5xl text-center py-8">
+          <span className="text-orange">Gallery</span> of our memories
+        </h2>
+        <Masonry {...masonryOptions}>
+          {galleryData.map(({ src }, key) => (
+            <div className="w-full">
+              <img
+                src={src}
+                alt={src}
+                key={key}
+                className="object-cover w-full h-full"
+              />
+            </div>
+          ))}
+        </Masonry>
+      </div>
+    </section>
+  );
+};
+
+export default GallerySection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import GallerySection from "../components/section/gallery-section";
 import GamesSection from "../components/section/games-section";
 import HeroSection from "../components/section/hero-section";
 import MemberSection from "../components/section/member-section";
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       <HeroSection />
       <GamesSection />
       <MemberSection />
+      <GallerySection />
     </div>
   );
 };
